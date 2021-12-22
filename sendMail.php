2904fdf2c34ad2146493296c6edbbfa787eb39f2
SanapPrasad2021 <95181420+SanapPrasad2021@users.noreply.github.com>
2021-12-22 19:26:34 +0530

    main

--- sendMail.php
+++ sendMail.php
@@ -4,7 +4,7 @@
         // Sending email
         $to=$email;
         $subject="Email Verification";
-        $message="<h5>Hey $fname $lname, you're almost ready to start enjoing<strong> XKCD Comics.</strong>Simply verify your email address.</h5><br><br><br><h3>Verification Key:<br>$vkey</h3><br><br><br><h5>Thank you.</h5>";
+        $message="<h3>Hey $fname $lname, you're almost ready to start enjoing<strong> XKCD Comics.</strong>Simply verify your email address.</h3><br><h3>Verification Key:<br>$vkey</h3><br><br><br><h5>Thank you.</h5>";
         $sender ="From: fmc202158@zealeducation.com\r\n";
         $sender .="MIME-Version: 1.0"."\r\n";
         $sender .="Content-type:text/html;charset=UTF-8"."\r\n";

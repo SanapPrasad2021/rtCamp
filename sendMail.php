888a3d1401a4ba757a31696161ab7170dad8b35b
SanapPrasad2021 <95181420+SanapPrasad2021@users.noreply.github.com>
2021-12-23 08:20:06 +0530

    Update sendMail.php

--- sendMail.php
+++ sendMail.php
@@ -2,16 +2,18 @@
     require_once 'databaseConnection.php'; 
     require_once 'config.php'; 
     require 'vendor/autoload.php';
-    function SendMail($fname,$lname,$email,$vkey){
-      // If you're using Composer (recommended)
-        $to=$email
+    function SendMail($fname,$lname,$email,$vkey)
+    {
+        // Sending email
+        $to=$email;
+        
         $email = new \SendGrid\Mail\Mail(); 
         $email->setFrom("fmc202158@zealeducation.com", "Prasad Sanap");
         $email->setSubject("XKCD Comics");
         $email->addTo("$to","User Details");
         $email->addContent("text/plain","This is message from XKCD Comics.");
         $email->addContent(
-            "text/html", "<h3>Hey <h1> $fname $lname</h1>, you're almost ready to start enjoing<h2> XKCD Comics.</h2>Simply verify your email address.</h3><br><h3>Verification Key:<br>$vkey</h3><br><br><br><h5>Thank you.</h5>"
+            "text/html", "<h3>Hey $fname $lname, you're almost ready to start enjoing<strong> XKCD Comics.</strong>Simply verify your email address.</h3><br><h3>Verification Key:<br>$vkey</h3><br><br><br><h5>Thank you.</h5>"
         );
         $sendgrid = new \SendGrid(SENDGRID_API_KEY);
         try {
@@ -23,7 +25,8 @@
         } catch (Exception $e) {
             echo 'Caught exception: '. $e->getMessage() ."\n";
         }
-       
+        
     }
+    
    
 ?>

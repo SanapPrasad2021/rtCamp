8fb16d6bce686f8dbfa4b3a52c440a69ba254a86
SanapPrasad2021 <95181420+SanapPrasad2021@users.noreply.github.com>
2021-12-24 18:44:37 +0530

    Update thankyou.php

--- thankyou.php
+++ thankyou.php
@@ -1,6 +1,8 @@
 <?php
     session_start();
 //     require_once 'sendMail.php';
+   require_once 'config.php'; 
+    require 'vendor/autoload.php';
     if(isset($_POST['goToVerify'])){
         header("location:verifyEmail.php");
     } 
